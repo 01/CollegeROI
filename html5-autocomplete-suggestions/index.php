c2da47f5df45ed20ace5ca8fd6e815a406224071
Andrew Khazanovich <akhazanovich+GIT@gmail.com>
2016-12-01 05:09:55 -0500

    Query builder in place

--- html5-autocomplete-suggestions/index.php
+++ html5-autocomplete-suggestions/index.php
@@ -10,6 +10,7 @@
   <script type="text/javascript" src="js/jquery.autocomplete.min.js"></script>
   <script type="text/javascript" src="js/currency-autocomplete.js"></script>
   <script type="text/javascript" src="js/addinput.js"></script>
+
 </head>
 <?php
 $servername = "127.0.0.1:3306";
@@ -96,11 +97,11 @@ $conn->close();
       <p>Please enter your college</p>
       
       <div id="searchfield">
-        <form><input type="text" name="currency" class="biginput" id="autocomplete"></form>
+        <form><input type="text" name="currency" class="biginput" id="collegeAuto"></form>
       </div><!-- @end #searchfield -->
       
       <div id="outputbox">
-        <p id="outputcontent">Choose a College and the results will display here.</p>
+        <p id="collegeOutput">Choose a College and the results will display here.</p>
       </div>
     </div><!-- @end #content -->
   </div><!-- @end #w -->
@@ -109,11 +110,11 @@ $conn->close();
       <p>Please enter your Major</p>
       
       <div id="searchfield">
-        <form><input type="text" name="currency" class="biginput" id="autocomplete2"></form>
+        <form><input type="text" name="currency" class="biginput" id="majorAuto"></form>
       </div><!-- @end #searchfield -->
       
       <div id="outputbox">
-        <p id="outputcontent2">Choose a Major and the results will display here.</p>
+        <p id="majorOutput">Choose a Major and the results will display here.</p>
       </div>
     </div><!-- @end #content -->
   </div><!-- @end #w -->
@@ -121,20 +122,28 @@ $conn->close();
     <div id="content" align="center">
       <p>Please enter your State</p>
       <div id="searchfield">
-        <form method="POST">
-          <div id="dynamicInput">
-            Entry 1<br><input type="text" class="biginput state-auto" name="myInputs[]">
-          </div>
-          <input type="button" value="Add another text input" onClick="addInput('dynamicInput');">
-
-
-        </form>
-
+  <form><input type="text" name="currency" class="biginput" id="stateAuto"></form>
       </div><!-- @end #searchfield -->
       
       <div id="outputbox">
-        <p id="outputcontent3">Choose a State and the results will display here.</p>
+        <p id="stateOutput">Choose a State and the results will display here.</p>
       </div>
+    </div><!-- @end #content -->
+  </div><!-- @end #w -->
+  <div id="w">
+    <div id="content" align="center">
+      <p>Please enter your State</p>
+      <div id="searchfield">
+<br>
+      <p>Submit?</p
+      <div id = "submitButton"><form><input type="button" value="submit" id="submit" ></div> 
+      
+      <p>
+         <label for = "price">Price range:</label>
+         <input type = "text" id = "price" 
+            style = "border:0; color:#b9cd6d; font-weight:bold;">
+      </p>
+      <div id = "slider-3"></div>
     </div><!-- @end #content -->
   </div><!-- @end #w -->
 
--- html5-autocomplete-suggestions/js/currency-autocomplete.js
+++ html5-autocomplete-suggestions/js/currency-autocomplete.js
@@ -1,48 +1,169 @@
+var stateArray = [];
+var majorArray = [];
+var collegeArray = [];
+var typeOfSchool = {
+  public: false,
+  privateNoProfit: false,
+  privateProfit: false
+};
+var size = {
+  small: false,
+  medium: false,
+  large: false
+};
+
+var submitQuery;
+var finalCollegeQueryString;
+var collegeQueryString;
+var stateQueryString;
+var majorsQueryString
+var sizeQueryString;
+var typeOfSchoolQueryString;
+var finalQueryString = "Select * From college.College_Directory";
+function createCollegeString (collegeArray){
+  if(collegeArray.length>0){
+      collegeQueryString = " ("
+      for (var i = 0; i <collegeArray.length-1; i++) {
+          collegeQueryString += ("INSTNM = '" + collegeArray[i] + "' OR ");
+      }
+      collegeQueryString += ("INSTNM = '" + collegeArray[i] + "')");
+      console.log(collegeQueryString);
+      console.log(collegeQueryString.length);
+      return collegeQueryString;
+
+    }
+}
+
+function createStateString(stateArray){
+  if(stateArray.length>0){
+      stateQueryString = " ("
+      for (var k = 0; k <stateArray.length-1; k++) {
+          stateQueryString += ("STABBR = '" + stateArray[k] + "' OR ");
+      }
+      stateQueryString += ("STABBR = '" + stateArray[k] + "')");
+      console.log(stateQueryString);
+      console.log(stateQueryString.length);
+      return stateQueryString;
+
+    }
+}
+
+function buildQueryString(submitQuery){
+  if(submitQuery.collegesString.length>0 | submitQuery.statesString.length>0)
+    finalQueryString+= " WHERE";
+  if(submitQuery.collegesString.length>0){
+    finalQueryString+= (" " + submitQuery.collegesString);
+    if(submitQuery.statesString.length>0){
+      finalQueryString += (" AND "+ submitQuery.statesString)
+    }
+  }
+  else if (submitQuery.statesString.length>0){
+    finalQueryString+=(" "+ submitQuery.statesString);
+  }
+  else{
+  }
+  console.log(finalQueryString);
+  return finalQueryString;
+}
+
+
+function queryString(collegesString, statesString) {
+  this.collegesString = collegesString;
+  //this.majorsStrings= majors;
+  this.statesString = statesString;
+  //this.sizeString = size;
+  //this.typeOfSchoolString = typeOfSchool;
+}
+
+/*function buildQueryString(submitQuery){
+    finalCollegeQueryString = "Select * From college.College_Directory";
+    if(submitQuery.colleges.length>0 | submitQuery.states.length>0 |(submitQuery.small|submitQuery.medium|submitQuery.large)|
+      (submitQuery.public|submitQuery.privateNoProfit|submitQuery.privateProfit)){
+        finalCollegeQueryString += " WHERE";
+    }
+    if(submitQuery.colleges.length>0){
+       finalCollegeQueryString += " ("
+      for (var k = 0; k <submitQuery.length-1; k++) {
+          finalCollegeQueryString += (" STABBR = '" + submitQuery.colleges[i] + "' OR");
+      }
+      finalCollegeQueryString += (" STABBR = '" + submitQuery.colleges[i] + "')");
+    }
+    elseif(submitQuery.states.length>0){
+      finalCollegeQueryString += " ("
+      for (var i = 0; i <submitQuery.length-1; i++) {
+          finalCollegeQueryString += (" STABBR = '" + submitQuery.colleges[i] + "' OR");
+      }
+      finalCollegeQueryString += (" STABBR = '" + submitQuery.colleges[i] + "')");
+    }
+  
+}*/
+
+function submit(){
+      collegeQueryString = createCollegeString(collegeArray);
+      console.log(collegeQueryString);
+      collegeArray = [];
+      stateQueryString = createStateString(stateArray);
+      console.log(stateQueryString);
+      stateArray=[];
+      submitQuery = new queryString(collegeQueryString, majorsQueryString, stateQueryString, sizeQueryString, typeOfSchoolQueryString);
+      finalQueryString = buildQueryString(submitQuery);
+      console.log(finalQueryString);
+      //submitQuery = new queryString(collegeArray, majorArray, stateArray, size, typeOfSchool);
+      //finalQueryString = buildQueryString(submitQuery);
+}
+
+
 $(function(){
- 
+
+
+    $('#submit').click(function(){
+       submit();
+    });
+
   // setup autocomplete function pulling from colleges[] array
-  $('#autocomplete').autocomplete({
+  $('#collegeAuto').autocomplete({
     lookup: colleges,
     onSelect: function (suggestion) {
-      var thehtml = '<strong>University:</strong>' + suggestion.INSTNM + ' <br> <strong>UNID:</strong> ' + suggestion.UNITID;
-      $('#outputcontent').html(thehtml);
+      var thehtml = '<strong>University:</strong>' + suggestion.value + ' <br> <strong>UNID:</strong> ' + suggestion.data;
+      $('#collegeOutput').html(thehtml);
+      collegeArray.push(suggestion.value);
+      console.log(collegeArray);
     }
   });
 
-  $('#autocomplete2').autocomplete({
+  $('#majorAuto').autocomplete({
     lookup: majors,
     onSelect: function (suggestion2) {
       var thehtml = '<strong>Major:</strong> ' + suggestion2.value + ' <br> <strong>Salary:</strong> ' + suggestion2.data;
-      $('#outputcontent2').html(thehtml);
+      $('#majorOutput').html(thehtml);
+      majorArray.push(suggestion2.value);
+      console.log(majorArray);
     }
   });
-  
-});
-$(function() {
+   $('#stateAuto').autocomplete({
+    lookup: states,
+    onSelect: function (suggestion) {
+      var thehtml = '<strong>State:</strong> ' + suggestion.value + ' <br> <strong>abreviation:</strong> ' + suggestion.data;
+      stateArray.push(suggestion.data);
+      console.log(stateArray);
+      $('#stateOutput').html(thehtml);
+    }
+  });
+
+
+  });
+/*
+ $(function(){
             $( "#slider-3" ).slider({
-               range:true,
+              range:true,
                min: 0,
-               max: 60000,
-               values: [ 15000, 25000 ],
+               max: 500,
+               values: [ 35, 200 ],
                slide: function( event, ui ) {
                   $( "#price" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
                }
             });
             $( "#price" ).val( "$" + $( "#slider-3" ).slider( "values", 0 ) +
-               " - $" + $( "#slider-3" ).slider( "values", 1 ) );
-         });
-         
-const stateInit = _ => {
-  $('.state-auto:not([init])').autocomplete({
-    lookup: states,
-    onSelect: function (suggestion) {
+             " - $" + $( "#slider-3" ).slider( "values", 1 ) );        });
+      */   
 
-      var thehtml = '<strong>State:</strong> ' + suggestion.value + ' <br> <strong>abreviation:</strong> ' + suggestion.data;
-      inputs.push(suggestion.data);
-      console.log(suggestion.data);
-      $('#outputcontent3').html(thehtml);
-    }
-  }).attr("init",true);
-}
-$(document).ready(_ => stateInit())
-$(document).ready(_ => inputs = new Array(0))
