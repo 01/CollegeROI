9f6e31a87d031fafb838d62f85d94b6ea4a6baf7
sswess <kotenkanto@gmail.com>
2016-12-06 18:01:37 -0500

    created 3 graphs

--- CollegeROI/collegecomparisons.php
+++ CollegeROI/collegecomparisons.php
@@ -1,3 +1,9 @@
+<?php include 'header.php'; ?>
+<?php include 'connect.php'; ?>
+<?php include 'sidebar.php'; ?>
+<?php include 'content.php'; ?>
+
+
 <!doctype html>
 <html lang="en-US">
 <head>
@@ -83,6 +89,13 @@
     // output data of each row
                 $netPriceNames= array();
                 $netPriceValues = array();
+                $AdmRate = array();
+                $salary1 = array();
+                $salary2 = array();
+                $salary3 = array();
+                $debt = array();
+                $debtM = array();
+                $debtF = array();
           
 ?>
 <script>
@@ -130,7 +143,28 @@ $(function () {
                 //echo "id: " . $row["INSTNM"]."<br>";
             $netPriceNames[]=$row["INSTNM"];
             if(!$row["Net_Price"])$netPriceValues[]=0;
-             else $netPriceValues[] = $row["Net_Price"];
+            else $netPriceValues[] = $row["Net_Price"];
+            if(!$row["ADM_RATE"])$AdmRate[]=0;
+            else $AdmRate[] = $row["ADM_RATE"]*100;
+            
+            if(!$row["MD_EARN_WNE_P6"])$salary1[]=0;
+            else $salary1[] = $row["MD_EARN_WNE_P6"];
+            if(!$row["MD_EARN_WNE_P8"])$salary2[]=0;
+            else $salary2[] = $row["MD_EARN_WNE_P8"];
+            if(!$row["MD_EARN_WNE_P10"])$salary3[]=0;
+            else $salary3[] = $row["MD_EARN_WNE_P10"];
+
+            if(!$row["DEBT_MDN"])$debt[]=0;
+            else $debt[] = $row["DEBT_MDN"];
+            if(!$row["FEMALE_DEBT_MDN"])$debtF[]=0;
+            else $debtF[] = $row["FEMALE_DEBT_MDN"];
+            if(!$row["MALE_DEBT_MDN"])$debtM[]=0;
+            else $debtM[] = $row["MALE_DEBT_MDN"];
+
+
+
+
+
            echo "{";
             echo "name: '".$row["INSTNM"]."',";
            echo "data: [".$row["NP_0_30"].",".$row["NP_30_48"].",". $row["NP_48_75"].",".$row["NP_75_110"].",".$row["NP_110_Plus"]."]";
@@ -195,6 +229,14 @@ $(function () {
 <div id="container"></div>
 <br><br><br>
 <div id="container1"></div>
+<br><br><br>
+<div id="container2"></div>
+<br><br><br>
+<div id="container4"></div>
+<br><br><br>
+<div id="container5"></div>
+
+
 <script type="text/javascript">
 
 $(function () {
@@ -203,6 +245,38 @@ $(function () {
                   labels: {
                       formatter: function() {
                           return '$' +this.value ;
+                        }
+                  }
+          },
+        title: {
+            text: 'Average Admission Rate comparison'
+        },
+        subtitle: {
+            text: 'Comparison of Admission Rate of Selected Colleges'
+        },
+        xAxis: {
+             categories: [<?php echo "'" .join($netPriceNames, "','"). "'" ?>]
+        },
+        series: [{
+            type: 'column',
+            colorByPoint: true,
+            data: [<?php echo join($netPriceValues, ', ') ?>],
+            showInLegend: false
+
+            
+        }]
+    });
+  })
+
+</script>
+<script type="text/javascript">
+
+$(function () {
+    var chart = Highcharts.chart('container2', {
+        yAxis: {
+                  labels: {
+                      formatter: function() {
+                          return  this.value+'%' ;
                         }
                   }
           },
@@ -218,7 +292,7 @@ $(function () {
         series: [{
             type: 'column',
             colorByPoint: true,
-            data: [<?php echo join($netPriceValues, ', ') ?>],
+            data: [<?php echo join($AdmRate, ', ') ?>],
             showInLegend: false
 
 
@@ -227,6 +301,112 @@ $(function () {
         }]
     });
   })
+$(function () {
+    Highcharts.chart('container4', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Salary Growth'
+        },
+        subtitle: {
+            text: 'Salary Growth'
+        },
+        xAxis: {
+            categories: [<?php echo "'" .join($netPriceNames, "','"). "'" ?>],
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Salary'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>${point.y:.1f}</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: '1 year',
+            data: [<?php echo join($salary1, ', ') ?>]
+
+        }, {
+            name: '3 year',
+            data: [<?php echo join($salary2, ', ') ?>]
+
+        }, {
+            name: '5 year',
+            data: [<?php echo join($salary3, ', ') ?>]
+
+        },]
+    });
+});
+
+
+$(function () {
+    Highcharts.chart('container5', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Expected Debt'
+        },
+        subtitle: {
+            text: 'Average Debt comparison of Female to Male'
+        },
+        xAxis: {
+            categories: [<?php echo "'" .join($netPriceNames, "','"). "'" ?>],
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Average Debt'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>${point.y:.1f}</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'Average',
+            data: [<?php echo join($debt, ', ') ?>],
+            color: 'grey'
+
+        }, {
+            name: 'Female',
+            data: [<?php echo join($debtM, ', ') ?>],
+            color: 'red'
+
+        }, {
+            name: 'Male',
+            data: [<?php echo join($debtF, ', ') ?>],
+            color: 'blue'
+
+
+        },]
+    });
+});
 
 </script>
 
