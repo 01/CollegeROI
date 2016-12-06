62533b2631a293e990b86c8912dbfa9d18139bf2
sswess <kotenkanto@gmail.com>
2016-12-05 22:02:42 -0500

    fix routing in sidebar and added the includes to form 3

--- CollegeROI/content.php
+++ CollegeROI/content.php
@@ -1,7 +1,7 @@
     <div id="page-content-wrapper" class="page-content-toggle">
-        <div class="container-fluid">            
+        <div class="container">            
 
             <div class="row">
                 <div id="content" class="col-md-8 col-md-offset-1 col-xs-12"><h1>Column Chart</h1>
-<div id="chartContainer"></div>
+	<div id="chartContainer"></div>
 
