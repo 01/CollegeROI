d62d32d96a05c672c542ec42c2c8ac620af74ca4
sswess <kotenkanto@gmail.com>
2016-12-06 16:39:39 -0500

    worked on top10.php

--- CollegeROI/content.php
+++ CollegeROI/content.php
@@ -1,7 +1,8 @@
     <div id="page-content-wrapper" class="page-content-toggle">
-        <div class="container ">            
+        <div class="container-fluid">            
 
             <div class="row">
-                <div id="content" class="col-md-8 col-md-offset-1 col-xs-12"><h1>Column Chart</h1>
+                <div id="content" class="col-md-10 col-md-offset-1 col-xs-12">
+                <h1>Column Chart</h1>
 	<div id="chartContainer"></div>
 
--- CollegeROI/top10.php
+++ CollegeROI/top10.php
@@ -5,17 +5,16 @@
 <h4>Popout</h4>
 
 
- <p class="caption">
-          To create a popout collapsible, just add the class <code class="language-markup">popout</code>.
-        </p>
         <ul class="collapsible popout collapsible-accordion" data-collapsible="accordion">
           <li>
-            <div class="collapsible-header"><i class="material-icons">subtitles</i>Top 10 Easiest and Hardest Schools to Get Into</div>
+            <div class="collapsible-header"><i class="material-icons">subtitles</i>Top 10 Easiest Schools to Get Into</div>
             <div class="collapsible-body">
-             <p> 
-             <pre class="tab">  <u>Top 10 Hardest Schools</u>                         <u>Top 10 Easiest Schools</u>
+             <p width = "30"> 
+             <pre class="tab">    <u>Top 10 Easiest Schools</u>                         
 <?php
 $debug = false;
+
+/********************Top 10 Admissions Easy and Hard ********************************************************************************/
 $top10AdmEasyQuery = "Select INSTNM, ADM_RATE From Main_Table WHERE (ADM_RATE < 1) ORDER BY ADM_RATE DESC LIMIT 10";
 $top10AdmHardQuery = "Select INSTNM, ADM_RATE From Main_Table WHERE (ADM_RATE > 0) ORDER BY ADM_RATE ASC LIMIT 10";
           
@@ -39,12 +38,7 @@ $top10AdmHardQuery = "Select INSTNM, ADM_RATE From Main_Table WHERE (ADM_RATE > 
               $schoolHard = $row2["INSTNM"];
               $admHard = $row2["ADM_RATE"] * 100;
              
-              echo '<p style="display:inline">';
-              if(i<10)echo "$i  ";
-              else echo "$i.";
-              echo $schoolHard;
-              echo " $admHard % </p>";
-              echo '                <p style="display:inline">';
+              echo '                <p style="display:">';
               echo $schoolEasy;
                echo " $admEasy % </p>";
               echo "</p>";
@@ -58,11 +52,52 @@ $top10AdmHardQuery = "Select INSTNM, ADM_RATE From Main_Table WHERE (ADM_RATE > 
         magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></div>
           </li>
           <li>
-            <div class="collapsible-header active"><i class="material-icons">games</i>Second</div>
-            <div class="collapsible-body"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></div>
+            <div class="collapsible-header"><i class="material-icons">subtitles</i>Top 10 Hardest Schools to Get Into</div>
+<div class="collapsible-body">
+             <p width = "30"> 
+             <pre class="tab">    <u>Top 10 Hardest Schools</u>
+<?php
+$debug = false;
+
+/********************Top 10 Admissions Easy and Hard ********************************************************************************/
+$top10AdmHardQuery = "Select INSTNM, ADM_RATE From Main_Table WHERE (ADM_RATE > 0) ORDER BY ADM_RATE ASC LIMIT 10";
+          
+          $top10EasyResult= $conn->query($top10AdmEasyQuery);
+          $top10HardResult= $conn->query($top10AdmHardQuery);
+          if($debug){
+                $top10EasyResult= $conn->query("SELECT * FROM Main_Table LIMIT 10");
+                $top10HardResult= $conn->query("SELECT * FROM Main_Table LIMIT 10");
+          }
+         
+          if(!$top10EasyResult|| !$top10HardResult){
+            echo "Could not find top 10 Admission Rates";
+          }
+          else{
+          $i = 1;
+            while($row1 =  $top10EasyResult->fetch_assoc()) {
+              $row2 = $top10HardResult->fetch_assoc();
+              
+
+              $schoolHard = $row2["INSTNM"];
+              $admHard = $row2["ADM_RATE"] * 100;
+             
+              echo '<p style="display:">'."$i.) $schoolHard";
+              echo " $admHard % </p>";
+              echo '                <p style="display:inline">';
+              echo "</p>";
+              $i++;
+            //  echo "<pre class="tab">1.) $schoolHard  $admHard %    $schoolEasy $admEasy % </pre><br>";
+               
+            }
+          }
+
+?></pre>
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></div>
           </li>
-          <li>
-            <div class="collapsible-header"><i class="material-icons">library_books</i>Third</div>
-            <div class="collapsible-body"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></div>
+
+
+
+
+
           </li>
         </ul>
