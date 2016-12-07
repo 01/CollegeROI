986465e16de1c8bc5272b491ea389cca271fb88c
sswess <kotenkanto@gmail.com>
2016-12-06 19:13:02 -0500

    created home page

---
+++ CollegeROI/home.php
@@ -0,0 +1,85 @@
+<?php include 'header.php'; ?>
+<?php include 'connect.php'; ?>
+<?php include 'sidebar.php'; ?>
+<?php include 'content.php'; ?>
+  <link href="css/bootstrap.min.css" rel="stylesheet">  
+
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="css/mystyles.css" rel="stylesheet">
+
+  <!-- icons -->
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/bootstrap-social.css" rel="stylesheet">
+
+  <!--JS -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+
+<div id="mypage" class="text-center">
+  <h3>The three amigos</h3>
+  <div class="row">
+    <div class="col-sm-4">
+      <p class="text-center"><strong>Andrew</strong></p><br>
+      <a href="#demo" data-toggle="collapse">
+        <img src="img/andrew.jpg" class="img-circle person" alt="Sample" width="255" height="255">
+      </a>
+        <p>Loves long walks on the beach</p>
+    </div>
+    <div class="col-sm-4">
+      <p class="text-center"><strong>Anton</strong></p><br>
+      <a href="#demo2" data-toggle="collapse">
+        <img src="img/anton.jpg" class="img-circle person" alt="Sample" width="255" height="255">
+      </a>
+        <p>Text 2</p>
+    </div>
+    <div class="col-sm-4">
+      <p class="text-center"><strong>Jesse</strong></p><br>
+      <a href="#demo3" data-toggle="collapse">
+        <img src="img/jesse.jpg" class="img-circle person" alt="Sample" width="255" height="255">
+      </a>
+        <p>Text 3</p>
+    </div>
+  </div>
+      <div class="row text-center">
+      <div class="row">
+        <p>To use our website Choose one of our forms</p>
+          <a class="btn btn-large btn-info" href="/form1.php">Form 1</a>
+          <br><br>
+          <a class="btn btn-large btn-info" href="/form2.php">Form 2</a>
+          <br><br>
+          <a class="btn btn-large btn-info" href="/form3.php">Form 3</a>
+      </div>
+    </div>
+</div>
+
+<!-- Container (TOUR Section) -->
+
+
+<!-- Container (Contact Section) -->
+
+  <h3 class="text-center">How to use our website</h3>
+  <p>random text random text random text random text random text random text random text random text random text random text random text random text </p>
+
+
+
+      
+  </div>
+
+
+
+<!-- Footer -->
+<footer class="text-center">
+  <a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a><br><br>
+  <p>Check us out on github</p>
+  <p>
+                        <a class="btn btn-social-icon btn-github" href="https://github.com/01" data-toggle="tooltip" title="Andrew Khaz"><i class="fa fa-github"></i></a>
+                        <a class="btn btn-social-icon btn-github" href="https://github.com/sswess" data-toggle="tooltip" title="Anton Kotenko"><i class="fa fa-github"></i></a>
+                        <a class="btn btn-social-icon btn-github" href="https://github.com/" data-toggle="tooltip" title="Jesse Striveli"><i class="fa fa-github"></i></a>
+                        
+            </p>       
+</footer>
